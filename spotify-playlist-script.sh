14fc147f493d2760df02b089db9df9f3cc8a2a6d
devkaare <kaarehofferdal@gmail.com>
2025-02-09 22:16:07 +0100

    Feat

--- spotify-playlist-script.sh
+++ spotify-playlist-script.sh
@@ -4,5 +4,5 @@ flatpak run com.spotify.Client &
 
 sleep 6
 
-playerctl --player=spotify open spotify:playlist:37i9dQZF1E4m5aoXVdJrlE
+playerctl --player=spotify open spotify:playlist:37i9dQZF1E8L4lSMtiCSfz
 playerctl --player=spotify play
